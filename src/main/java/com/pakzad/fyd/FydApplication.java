2fe277911eba071852dc296bcf34ebefb795d89e
RamPak <ramin.highborn@yahoo.com>
2018-07-13 18:33:23 +0430

    remove wrong package

---
+++ src/main/java/com/pakzad/fyd/FydApplication.java
@@ -0,0 +1,11 @@
+package com.pakzad.fyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FydApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FydApplication.class, args);
+    }
+}
