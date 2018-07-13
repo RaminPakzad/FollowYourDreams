f599218eee9592c3a1b9a34dcb0bd9a4f4e16e36
RamPak <ramin.highborn@yahoo.com>
2018-07-13 17:59:58 +0430

    initial commit

---
+++ src/main/java/com/pakzad/dream/fyd/FydApplication.java
@@ -0,0 +1,12 @@
+package com.pakzad.dream.fyd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FydApplication.class, args);
+	}
+}
